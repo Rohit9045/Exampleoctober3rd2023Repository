ff09c7f59e9f0522634dab3800c21c0e2b5a1bd9
Rohit9045 <rraikar60@gmail.com>
2023-11-10 18:04:22 +0530

    sg programs

---
+++ Demojavaproject/src/demo1/Sample1.java
@@ -0,0 +1,15 @@
+package demo1;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+			char ch[]= {'a','b','c','d','e','f'};
+		     int i=0;
+		     while(i<=ch.length-1) {
+		     	System.out.println(ch[i]);
+		     	i++;
+		}
+
+	}
+
+}
